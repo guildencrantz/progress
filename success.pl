5fcaa2d7eccb9235a3fe58f258695530798903ea
Matt Henkel <matt.henkel@returnpath.com>
2013-04-02 11:36:15 -0600

    More configs.
    Adding after and before flags to be passed to git-log.
    
    Adding gravatar_dir flag.
    
    Adding glob flag so that repos can be matched by perl glob.
    
    Adding flag to skip repo fetching.
    Fixing fetching so that we get the log from the remote branch.
    Also adding the ability to figure out what repo, and branch, master is
    tracking for getting the git log.
    
    Adding flag to skip retrieving gravatars.
    
    Now calculating seconds per day based on range of actual commits, not
    requested dates (tightens up the video if there aren't any commits in
    the first, or last, days).
    
    Dropping mp constant rate from 1 to 23 (1 minute videos are now around
    10MB).
    
    Change-Id: I03d14d9beba9867ef9100d64259e04dbc3e35a37

--- success.pl
+++ success.pl
@@ -16,42 +16,66 @@ use LWP::Simple;
 
 my @repositories;
 my $repo_glob;
+my $after;
+my $before;
+my $no_fetch;
+my $avatar_output_dir = '.avatar';
+my $no_gravatar;
 
 GetOptions(
+  'a|after=s'      => \$after,
+  'b|before=s'     => \$before,
+  'd|gravatar_dir' => \$avatar_output_dir,
+  'g|glob=s'       => \$repo_glob,
+  'no_fetch'       => \$no_fetch,
+  'no_gravatar'    => \$no_gravatar,
   'r|repository=s' => \@repositories,
-  'g|glob=s'       => \$repo_glob
 );
 
 push(@repositories, glob($repo_glob)) if defined $repo_glob;
 
+my @git_flags;
+push(@git_flags, "--after=${after}") if defined $after;
+push(@git_flags, "--before=${before}") if defined $before;
+
 my $avatar_size       = 90;
-my $avatar_output_dir = '.gravatars';
 my $outfile = 'gource.mp4';
-my $RESOLUTION = '1024x768';
-#my $RESOLUTION = '640x480';
+my $resolution = '1024x768';
 my $target_length_seconds = 60;
-my $total_days = 14;
 
 mkdir($avatar_output_dir) unless -d $avatar_output_dir;
-
-# TODO: Dynamicize
-my $repo_name = 'auth_engine_aggregator';
-my $since = '14.days';
 
 my %processed_authors;
 my @combined_log;
 
 foreach my $repo_name (@repositories) {
-  my $repo = Git::Repository->new( git_dir => "/home/mhenkel/src/rp/$repo_name/.git" );
-  $repo->run('fetch', '--all');
+  print "Processing ${repo_name}\n";
 
-  my $repo_log_iterator = Git::Repository::Log::Iterator->new($repo, "master", "--name-status", "--since=${since}");
+  my $repo = Git::Repository->new( git_dir => "/home/mhenkel/src/rp/$repo_name/.git" );
+  # TODO: Allow passing the remote_branch on the CLI
+  my $remote_branch;
+  unless(defined $no_fetch) {
+    my $remote_repo;
+    foreach ($repo->run('branch', '-vv')) {
+      # If the local branch isn't in sync with the remote there's a colon
+      # and description of the state difference after the remote branch path
+      if(/master\s+\w+\s+\[(([^\/]+?)\/[^\]\:]+).*?\].+/) {
+        $remote_branch = $1;
+        $remote_repo = $2;
+      }
+    }
+    defined $remote_repo ? $repo->run('fetch', $remote_repo) : next;
+  }
+
+  my $repo_log_iterator = Git::Repository::Log::Iterator->new($repo, $remote_branch, "--name-status", @git_flags);
 
   while (my $log = $repo_log_iterator->next) {
-    unless ($processed_authors{$log->author_name}++) {
+    unless ($no_gravatar || $processed_authors{$log->author_name}++) {
 
       my $author_image_file = $avatar_output_dir . '/' . $log->author_name . '.png';
       if (! -e $author_image_file) {
+        printf "Checking for gravatar for %s\n", $log->author_name;
+
         my $grav_url = sprintf(
           'http://www.gravatar.com/avatar/%s?d=404&size=%i',
           md5_hex(lc $log->author_email),
@@ -83,14 +107,6 @@ foreach my $repo_name (@repositories) {
 
 @combined_log = sort @combined_log;
 
-##gource will not close when reading from a pipe like this.
-#my $gource_pid = open(GOURCE, "| gource - --log-format custom --background-image ~/src/success/success.png -s ${seconds_per_day}  -i 0 -$RESOLUTION --highlight-users --highlight-dirs --hide mouse --key --stop-at-end --user-image-dir $avatar_output_dir --output-framerate 25 --output-ppm-stream - | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -crf 1 -threads 0 -bf 0 $outfile") or die "Unable to open gource: $!\n";
-#
-#print GOURCE @combined_log;
-#print GOURCE "\n";
-#
-#close GOURCE;
-
 my $combined_log_path = "/tmp/success_combined_log.$$";
 open (COMBINED_LOG, '>', $combined_log_path);
 print COMBINED_LOG @combined_log;
@@ -98,12 +114,11 @@ close COMBINED_LOG;
 
 my $first_day_with_data = DateTime->from_epoch(epoch => (split /\|/, $combined_log[0])[0]);
 my $last_day_with_data = DateTime->from_epoch(epoch => (split /\|/, $combined_log[-1])[0]);
-my $actual_days = $first_day_with_data->delta_days($last_day_with_data)->days;
+my $actual_days = $first_day_with_data->delta_days($last_day_with_data)->in_units('days');
 
 my $seconds_per_day = $target_length_seconds / $actual_days;
 
-#`gource $combined_log_path --log-format custom --background-image ~/src/success/success.png -s ${seconds_per_day}  -i 0 -$RESOLUTION --highlight-users --highlight-dirs --hide mouse --key --stop-at-end --user-image-dir $avatar_output_dir --output-framerate 25 --output-ppm-stream - | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -crf 1 -threads 0 -bf 0 $outfile` or die "Unable to generate movie: $!\n";
-`xvfb-run -a -s "-screen 0 ${RESOLUTION}x24" gource $combined_log_path --log-format custom --background-image ~/src/success/success.png -s ${seconds_per_day}  -i 0 -$RESOLUTION --highlight-users --highlight-dirs --hide mouse --key --stop-at-end --user-image-dir $avatar_output_dir --output-framerate 25 --output-ppm-stream - | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -crf 1 -threads 0 -bf 0 $outfile` or die "Unable to generate movie: $!\n";
+`xvfb-run -a -s "-screen 0 ${resolution}x24" gource ${combined_log_path} --log-format custom --background-image ~/src/success/success.png -s ${seconds_per_day}  -i 0 -${resolution} --highlight-users --highlight-dirs --hide mouse --key --stop-at-end --user-image-dir ${avatar_output_dir} --output-framerate 25 --output-ppm-stream - | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset medium -crf 23 -threads 0 -bf 0 ${outfile}` or die "Unable to generate movie: $!\n";
 
 print "Done.\n";
 
