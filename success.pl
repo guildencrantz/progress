4ae127e873cf645873255c6200cd16881212735c
Matt Henkel <matt.henkel@returnpath.com>
2013-04-03 12:08:55 -0600

    Cleaning up a bit.
    Removed repository path so it now works anywhere.
    
    Now defaults to reading the local master if no remote is found.
    
    If there aren't any files inthe log a warning is written rather than
    trying to process nothing.
    
    Change-Id: I33405fb9d0666d22e871f6dfc04692f86353194c

--- success.pl
+++ success.pl
@@ -61,24 +61,24 @@ my @combined_log;
 foreach my $repo_name (@repositories) {
   print "Processing ${repo_name}\n";
 
-  my $repo = Git::Repository->new( git_dir => "/home/mhenkel/src/rp/$repo_name/.git" );
+  my $repo = Git::Repository->new( git_dir => "$repo_name/.git" );
   # TODO: Allow passing the remote_branch on the CLI
-  my $remote_branch;
+  my $branch;
   my $remote_repo;
   foreach ($repo->run('branch', '-vv')) {
     # If the local branch isn't in sync with the remote there's a colon
     # and description of the state difference after the remote branch path
     if(/master\s+\w+\s+\[(([^\/]+?)\/[^\]\:]+).*?\].+/) {
-      $remote_branch = $1;
+      $branch = $1;
       $remote_repo = $2;
     }
   }
-  unless(defined $no_fetch) {
-    my $remote_repo;
+  unless(defined $no_fetch || ! defined $remote_repo) {
     defined $remote_repo ? $repo->run('fetch', $remote_repo) : next;
   }
 
-  my $repo_log_iterator = Git::Repository::Log::Iterator->new($repo, $remote_branch, "--name-status", @git_flags);
+  $branch = 'master' unless defined $branch;
+  my $repo_log_iterator = Git::Repository::Log::Iterator->new($repo, $branch, "--name-status", @git_flags);
 
   while (my $log = $repo_log_iterator->next) {
     unless ($no_gravatar || $processed_authors{$log->author_name}++) {
@@ -116,20 +116,24 @@ foreach my $repo_name (@repositories) {
   }
 }
 
-@combined_log = sort @combined_log;
+if ($#combined_log >= 0) {
+  @combined_log = sort @combined_log;
+  my $combined_log_path = "/tmp/success_combined_log.$$";
+  open (COMBINED_LOG, '>', $combined_log_path);
+  print COMBINED_LOG @combined_log;
+  close COMBINED_LOG;
 
-my $combined_log_path = "/tmp/success_combined_log.$$";
-open (COMBINED_LOG, '>', $combined_log_path);
-print COMBINED_LOG @combined_log;
-close COMBINED_LOG;
+  my $first_day_with_data = DateTime->from_epoch(epoch => (split /\|/, $combined_log[0])[0]);
+  my $last_day_with_data = DateTime->from_epoch(epoch => (split /\|/, $combined_log[-1])[0]);
+  my $actual_days = $first_day_with_data->delta_days($last_day_with_data)->in_units('days');
+  $actual_days = $actual_days == 0 ? 1 : $actual_days;
 
-my $first_day_with_data = DateTime->from_epoch(epoch => (split /\|/, $combined_log[0])[0]);
-my $last_day_with_data = DateTime->from_epoch(epoch => (split /\|/, $combined_log[-1])[0]);
-my $actual_days = $first_day_with_data->delta_days($last_day_with_data)->in_units('days');
+  my $seconds_per_day = $target_length_seconds / $actual_days;
 
-my $seconds_per_day = $target_length_seconds / $actual_days;
-
-`xvfb-run -a -s "-screen 0 ${resolution}x24" gource ${combined_log_path} --log-format custom --background-image ~/src/success/success.png -s ${seconds_per_day}  -i 0 -${resolution} --highlight-users --highlight-dirs --hide mouse --key --stop-at-end --user-image-dir ${avatar_output_dir} --output-framerate 25 ${title} --output-ppm-stream - | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset medium -crf 23 -threads 0 -bf 0 ${outfile}` or die "Unable to generate movie: $!\n";
+  `xvfb-run -a -s "-screen 0 ${resolution}x24" gource ${combined_log_path} --log-format custom --background-image ~/src/success/success.png -s ${seconds_per_day}  -i 0 -${resolution} --highlight-users --highlight-dirs --hide mouse --key --stop-at-end --user-image-dir ${avatar_output_dir} --output-framerate 25 ${title} --output-ppm-stream - | ffmpeg -y -r 25 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset medium -crf 23 -threads 0 -bf 0 ${outfile}` or die "Unable to generate movie: $!\n";
+} else {
+  warn "No commits found to visualize.";
+}
 
 print "Done.\n";
 
